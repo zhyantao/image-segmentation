a3d72bf34fc994a1112b5b2e561cf0abf177ba49
toooney <toooney@126.com>
2019-04-28 21:50:34 +0800

    retinal segmentation

---
+++ test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from model_v3 import *
+from data import *
+import numpy as np
+import cv2
+import os
+import warnings
+from PIL import Image
+
+warnings.filterwarnings("ignore")
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+def image_normalized(file_path):
+    '''
+    png, size:512*512，grayscale
+    :param dir_path: path to your images directory
+    :return:
+    '''
+    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    img_shape = img.shape
+    image_size = (img_shape[1],img_shape[0])
+    img_standard = cv2.resize(img, (512, 512), interpolation=cv2.INTER_CUBIC)
+    img_new = img_standard
+    img_new = np.asarray([img_new / 255.])
+    img_new = img_new[:, :, :, np.newaxis] # add 1 dimension
+    return img_new,image_size
+
+
+if __name__ == '__main__':
+
+    #path to images which aring wating for predicting
+    test_path = "data/test/"
+
+    # save the predict images
+    save_path = "data/predict/"
+
+    dp = data_preprocess(test_path=test_path,save_path=save_path,flag_multi_class=False,num_classes=2)
+
+    #load model
+    model = load_model('weights/model_v3.hdf5')
+
+    for name in os.listdir(test_path):
+        image_path = os.path.join(test_path,name)
+        x,img_size = image_normalized(image_path)
+        results = model.predict(x)
+        dp.saveResult([results[0]],img_size,name.split('.')[0])
+        print(' Picture ' + name + ' predict complete.')
---
+++ train.py
@@ -0,0 +1,64 @@
+#encoding:utf-8
+#!/usr/bin/env python3
+
+from model_v3 import  *
+from data import *
+import os
+import keras
+from keras.callbacks import TensorBoard
+import tensorflow as tf
+import keras.backend.tensorflow_backend as K
+import matplotlib.pyplot as plt
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+K.set_session(sess)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+if __name__ == '__main__':
+
+    #path to images which are prepared to train a model
+    train_path = "data"
+    image_folder = "train"
+    label_folder = "train_label"
+    valid_path =  "data"
+    valid_image_folder ="val"
+    valid_label_folder = "val_label"
+    log_filepath = 'log'
+    flag_multi_class = False
+    num_classes = 2
+    dp = data_preprocess(train_path=train_path,image_folder=image_folder,label_folder=label_folder,
+                         valid_path=valid_path,valid_image_folder=valid_image_folder,valid_label_folder=valid_label_folder,
+                         flag_multi_class=flag_multi_class,
+                         num_classes=num_classes)
+
+    # train your own model
+    train_data = dp.trainGenerator(batch_size=2)
+    valid_data = dp.validLoad(batch_size=2)
+    test_data = dp.testGenerator()
+    model = unet(num_class=num_classes)
+
+    tb_cb = TensorBoard(log_dir=log_filepath)
+    model_checkpoint = keras.callbacks.ModelCheckpoint('weights/model_v3.hdf5', monitor='val_loss',verbose=1,save_best_only=True)
+    history = model.fit_generator(train_data,
+                                  steps_per_epoch=200,epochs=30,
+                                  validation_steps=20,
+                                  validation_data=valid_data,
+                                  callbacks=[model_checkpoint,tb_cb])
+
+    # draw the loss and accuracy curve
+    plt.figure(12, figsize=(6, 6), dpi=60)
+    plt.subplot(211)
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='val')
+    plt.title('loss')
+    plt.legend()
+
+    plt.subplot(212)
+    plt.plot(history.history['acc'], label='train')
+    plt.plot(history.history['val_acc'], label='val')
+    plt.title('acc')
+    plt.legend()
+
+    plt.show()
