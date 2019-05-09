1f93898248820b6d62333bb3ca4219466243ef74
toooney <toooney@126.com>
2019-05-09 21:34:59 +0800

    change model input shape to 64*720 and del add_color

--- label_image.py
+++ label_image.py
@@ -18,30 +18,18 @@ class LabelImage:
         image_array = np.array(image)
         image_array_height = image_array.shape[0]
         image_array_width = image_array.shape[1]
-        #w_flag_label = []
-        #w_flag_nlabel = []
         for w in range(0, image_array_width):
-           # h_flag_label = 0
-           # h_flag_nlabel = 0
             for h in range(0, image_array_height):
                 if w >= self.col_start and w <= self.col_end:
                     if image_array[h, w] < 90:  # segmentation of vulnerable plaques and background thresholds
                         image_array[h, w] = 0   # background: Black
                     else:
                         image_array[h, w] = 255 # is label
-                        #w_flag_label.append(w)
-                        #if h > h_flag_label:
-                        #    h_flag_label = h
                 elif w >= self.flag:
                     if image_array[h, w] < 60:  # segmentation of vulnerable plaques and background thresholds
                         image_array[h, w] = 0   # background: Black
                     else:
                         image_array[h, w] = 128 # is NOT label
-                        #w_flag_nlabel.append(w)
-                        #if h > h_flag_nlabel:
-                        #    h_flag_nlabel = h
-            #image_array[0:h_flag_nlabel, w_flag_nlabel] = 128 # is NOT label
-            #image_array[0:h_flag_label, w_flag_label] = 255   # is label
         label_image = Image.fromarray(np.uint8(image_array))
         # save labeled image
         label_image.save(os.path.join(self.file_folder, os.path.basename(self.file_name)))
