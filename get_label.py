377ac4feec734202aa34b59ec2a71aa339c994bf
zhyantao <yantao.z@outlook.com>
2019-05-11 00:49:44 +0800

    fix label_images

--- get_label.py
+++ get_label.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-from label_image import LabelImage
 import re
+import numpy as np
+from PIL import Image
+import os
+from label_image import LabelImage
 
 label_image_folder = 'data/original/labels/'
 
@@ -30,13 +33,19 @@ class GetLabel:
             if count < 1000:
                 for i in range(2, len_list_item):
                     # mask the image
-                    list_item_tmp = re.findall(r"\d+", list_item[i]) # use regular expression to remove \n
-                    col_start = int(list_item_tmp[0])
-                    col_end = int(list_item_tmp[1])
+                    item_label = re.split(' *', ' '.join(list_item[2:]))
+                    item_label_last = re.findall(r"\d+", item_label[-1])
+                    item_label[-1] = item_label_last[-1]
+                    sort_item_label = sorted(list(map(int, item_label)))
+                    col_start = sort_item_label[2*(i-2)]
+                    col_end = sort_item_label[2*(i-2)+1]
                     label_image = LabelImage(file_folder = label_image_folder, file_name = label_file_name, col_start = col_start, col_end = col_end, flag = flag)
                     label_image.labelImage()
                     flag = col_end # refresh flag
             else:
                 label_image = LabelImage(file_folder = label_image_folder, file_name = label_file_name, col_start = col_start, col_end = col_end, flag = flag)
                 label_image.labelImage()
+            labeled_image = np.array(Image.open(label_image_folder + label_file_name))/127
+            labeled_image = Image.fromarray(np.uint8(labeled_image))
+            labeled_image.save(os.path.join(label_image_folder, os.path.basename(label_file_name)))
             count += 1
